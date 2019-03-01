688a43d97abc1eab7349d2fe4cea708415b6d2ba
RainFool <rainfool1024@gmail.com>
2019-03-01 14:49:11 +0800

    Initial commit

---
+++ buildSrc/src/main/java/com.duowan.viewcomponent.plugin/rfinal/Drive.kt
@@ -0,0 +1,17 @@
+package com.duowan.viewcomponent.plugin.rfinal
+
+/**
+ *
+ *
+ * @author rainfool
+ * @date 2019/2/28
+ */
+object Drive {
+    init {
+        Test.test()
+    }
+
+    fun drive() {
+
+    }
+}
---
+++ buildSrc/src/main/java/com.duowan.viewcomponent.plugin/rfinal/Test.java
@@ -0,0 +1,10 @@
+package com.duowan.viewcomponent.plugin.rfinal;
+
+/**
+ * @author rainfool
+ * @date 2019/2/28
+ */
+public class Test {
+    public static void test() {
+    }
+}
